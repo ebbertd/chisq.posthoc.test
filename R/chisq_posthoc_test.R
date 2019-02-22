f10f0c8f6986e2a62dc52d2e7a961760fdd794ff
Daniel Ebbert <daniel@ebbert.nl>
2019-02-22 14:50:50 +0100

    Combined residuals and p.values into one table

--- R/chisq_posthoc_test.R
+++ R/chisq_posthoc_test.R
@@ -10,7 +10,11 @@
 #' @export
 
 chisq.posthoc.test <-
-  function(x, y, method = "bonferroni", round = 6, ...) {
+  function(x,
+           y,
+           method = "bonferroni",
+           round = 6,
+           ...) {
     # Perform the chi square test and save the residuals
     stdres <- chisq.test(x, y, ...)$stdres
     # Calculate the chi square values based on the residuls
@@ -20,13 +24,30 @@ chisq.posthoc.test <-
     # Adjust the p values with the chosen method
     adjusted_p_values <- p_values
     for (i in 1:nrow(adjusted_p_values)) {
-      adjusted_p_values[i, ] <- p.adjust(
-        adjusted_p_values[i, ],
+      adjusted_p_values[i,] <- p.adjust(
+        adjusted_p_values[i,],
         method = method,
         n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
       )
     }
+    # Round the adjusted p values
     adjusted_p_values <- round(adjusted_p_values, digits = round)
+    # Convert stdres and adjusted p values into data frames
+    stdres <- as.data.frame.matrix(stdres)
+    adjusted_p_values <- as.data.frame.matrix(adjusted_p_values)
+    # Combine residuals and p values into one table
+    results <-
+      as.data.frame(matrix(
+        data = NA,
+        nrow = nrow(adjusted_p_values) * 2,
+        ncol = ncol(adjusted_p_values) + 1
+      ))
+    odd_rows <- seq(1, nrow(results), 2)
+    even_rows <- seq(2, nrow(results), 2)
+    results[odd_rows, c(2:ncol(results))] <- stdres
+    results[even_rows, c(2:ncol(results))] <- adjusted_p_values
+    results[odd_rows, 1] <- "Residuals"
+    results[even_rows, 1] <- "p values"
     # Print the results
-    print(adjusted_p_values)
+    print(results)
   }
